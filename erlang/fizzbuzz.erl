e1c9381f00a67ff36d8662574b75e937ec9632f3
hugo <hugo.cavan2386@gmail.com>
2020-07-18 01:38:06 +0800

    [erlang]Guards

--- erlang/fizzbuzz.erl
+++ erlang/fizzbuzz.erl
@@ -3,11 +3,16 @@
 -export([fizzbuzz/1]).
 
 fizzbuzz(N) when N =/= 0 ->
-    fizzbuzz(N - 1),
-    Saying = if N rem 3 == 0, N rem 5 == 0 -> "fizzbuzz";
-		N rem 3 == 0 -> "fizz";
-		N rem 5 == 0 -> "buzz";
-		true -> integer_to_list(N)
-	     end,
-    io:format('~s~n', [Saying]);
+	fizzbuzz(N-1),
+	saying(N);
+
 fizzbuzz(_) -> true.
+
+saying(N) when N rem 3 == 0, N rem 5 == 0 ->
+	io:format('~p,~s~n', [N, "fizzbuzz"]);
+saying(N) when N rem 3 == 0 ->
+	io:format('~p,~s~n', [N, "fizz"]);
+saying(N) when N rem 5 == 0 ->
+	io:format('~p,~s~n', [N, "buzz"]);
+saying(N) ->
+	io:format('~s~n', [integer_to_list(N)]).
