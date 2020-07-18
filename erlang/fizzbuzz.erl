f23b6ff888aed804404f722a3198875271bde40c
hugo <hugo.cavan2386@gmail.com>
2020-07-18 18:45:44 +0800

    [erlang]

--- erlang/fizzbuzz.erl
+++ erlang/fizzbuzz.erl
@@ -2,17 +2,51 @@
 
 -export([fizzbuzz/1]).
 
-fizzbuzz(N) when N =/= 0 ->
-	fizzbuzz(N-1),
-	saying(N);
+fizzbuzz(N) -> fizzbuzz(N, spec()).
 
-fizzbuzz(_) -> true.
+fizzbuzz(0, _) -> ok;
+fizzbuzz(N, R) -> fizzbuzz(N - 1, R), R(N).
 
-saying(N) when N rem 3 == 0, N rem 5 == 0 ->
-	io:format('~p,~s~n', [N, "fizzbuzz"]);
-saying(N) when N rem 3 == 0 ->
-	io:format('~p,~s~n', [N, "fizz"]);
-saying(N) when N rem 5 == 0 ->
-	io:format('~p,~s~n', [N, "buzz"]);
-saying(N) ->
-	io:format('~s~n', [integer_to_list(N)]).
+spec() ->
+    R0 = all([times(3), times(5)]),
+    R = any([rule(R0, saying("fizzbuzz")),
+	     rule(times(3), saying("fizz")),
+	     rule(times(5), saying("buzz")),
+	     rule(always(), saying())]),
+    fun (N) -> R(N) end.
+
+any([]) -> fun (_) -> false end;
+any(L) -> fun (N) -> any(N, L) end.
+
+any(_, []) -> false;
+any(N, [H | T]) ->
+    case H(N) of
+      true -> true;
+      false -> any(N, T)
+    end.
+
+all([]) -> fun (_) -> false end;
+all(L) -> fun (N) -> all(N, L) end.
+
+all(_, []) -> true;
+all(N, [H | T]) ->
+    case H(N) of
+      true -> all(N, T);
+      false -> false
+    end.
+
+times(X) -> fun (N) -> N rem X =:= 0 end.
+
+always() -> fun (_) -> true end.
+
+rule(Pred, Action) ->
+    fun (N) ->
+	    case Pred(N) of
+	      true -> Action(N), true;
+	      false -> false
+	    end
+    end.
+
+saying() -> fun (N) -> io:format("~B~n", [N]) end.
+
+saying(X) -> fun (_) -> io:format("~s~n", [X]) end.
